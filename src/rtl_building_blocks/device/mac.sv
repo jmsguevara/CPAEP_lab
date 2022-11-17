30e6ffcf097e9bfe6af2c2a3ec018bf6abb56e63
yz_esat <yz_esat>
2022-11-17 17:37:30 +0100

    attempt to add pipelining in MAC

--- src/rtl_building_blocks/device/mac.sv
+++ src/rtl_building_blocks/device/mac.sv
@@ -35,6 +35,18 @@ module mac #(
                         clk
   */
 
+//   // Pipelining: MEM
+
+//   `REG(A_WIDTH, pp_a);
+//   assign pp_a_we = input_valid;
+//   assign pp_a_next = a;
+
+//   `REG(B_WIDTH, pp_b);
+//   assign pp_b_we = input_valid;
+//   assign pp_b_next = b;
+
+//   // Pipelining: MEM -> EXEC
+
   logic signed [ACCUMULATOR_WIDTH-1:0] product;
   multiplier #( .A_WIDTH(A_WIDTH),
                 .B_WIDTH(B_WIDTH),
