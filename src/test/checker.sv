204f02a1bec79853a74f3215277050dff1461fe3
YZ <charliehe0211@gmail.com>
2022-11-18 01:00:29 +0100

    sync

--- src/test/checker.sv
+++ src/test/checker.sv
@@ -118,7 +118,7 @@ class Checker #(config_t cfg);
           if (verbose) $display("[CHK] Result is correct");
         end else begin
           $display("[CHK] Result is incorrect");
-          $display("expected = %d %% ", expected);
+          $display("expected = %h", expected);
           $stop;
         end
         count++;
